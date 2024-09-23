50aadba7c712eabba83b46b03a715909d35c1f8d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-23 14:30:51 +0200

    feat: add routes in app.js

--- api/app.js
+++ api/app.js
@@ -5,4 +5,8 @@ const routes = require('./routes/routes');
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/auth', routes.auth);
+app.use('/users', routes.user);
+app.use('/posts', routes.post);
+
 app.listen(process.env.PORT);
