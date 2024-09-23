4744a28aad5c504e93caf790006d84e1b41ecf58
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-23 15:47:45 +0200

    feat: add comment route

--- api/app.js
+++ api/app.js
@@ -8,5 +8,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/auth', routes.auth);
 app.use('/users', routes.user);
 app.use('/posts', routes.post);
+app.use('/comments', routes.comment);
 
 app.listen(process.env.PORT);
