0c30832b1cb401a43b538b360e0e273223369daf
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-23 17:28:50 +0200

    feat: add signUp function

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,34 @@
-const signUp = (req, res) => {};
+require('dotenv').config();
+const { body } = require('express-validator');
+const bcrypt = require('bcrypt');
+const prisma = require('../models/queries');
+
+const validateUser = [
+  body('username').trim().blacklist('<>'),
+  body('email').trim().blacklist('<>'),
+  body('password').trim(),
+  body('role').trim(),
+];
+
+const signUp = [
+  validateUser,
+  (req, res) => {
+    const role = req.body.role === process.env.AUTHOR_PWD ? 'AUTHOR' : 'USER';
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(503);
+      }
+      await prisma.createUser({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        role,
+      });
+      res.sendStatus(201);
+    });
+  },
+];
 
 const logIn = (req, res) => {};
 
