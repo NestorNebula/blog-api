45a2e8d72343b5e74b5a28242a6d643dedf811fa
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-24 12:01:03 +0200

    feat: add passport jwt-strategy

--- api/utils/passport.js
+++ api/utils/passport.js
@@ -1,1 +1,18 @@
+require('dotenv').config();
 const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const prisma = require('../models/queries');
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.AT,
+};
+passport.use(
+  new Strategy(opts, async (jwt_payload, done) => {
+    const user = await prisma.getUserById(jwt_payload.id);
+    if (!user) {
+      return done(null, false);
+    }
+    return done(null, user);
+  })
+);
