6127438a91eca5ec2c0e6c6b2087073888f2c76f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-24 18:29:05 +0200

    feat: add getPosts and postPost functions

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,6 +1,24 @@
-const getPosts = (req, res) => {};
+const prisma = require('../models/queries');
+const { validatePost } = require('../utils/validation');
 
-const postPost = (req, res) => {};
+const getPosts = async (req, res) => {
+  const posts = await prisma.getAllPosts();
+  res.json(posts);
+};
+
+const postPost = [
+  validatePost,
+  async (req, res) => {
+    if (req.user.role !== 'AUTHOR') return res.sendStatus(403);
+    await prisma.createPost({
+      userId: req.user.id,
+      title: req.body.title,
+      content: req.body.content,
+      published: req.body.published ? true : false,
+    });
+    res.sendStatus(201);
+  },
+];
 
 const getPost = (req, res) => {};
 
