7f64c3c1a9af8d69ed0bde3e0c0116db556e69f8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-24 17:55:48 +0200

    feat: protect user route with passport

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ const app = express();
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const routes = require('./routes/routes');
+const passport = require('passport');
 require('./utils/passport');
 
 app.use(express.json());
@@ -10,7 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/auth', routes.auth);
-app.use('/users', routes.user);
+app.use(
+  '/users',
+  passport.authenticate('jwt', { session: false }),
+  routes.user
+);
 app.use('/posts', routes.post);
 app.use('/comments', routes.comment);
 
