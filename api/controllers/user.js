1feee2a3428fbba7b79efd52d90f521906035b46
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-24 17:57:24 +0200

    feat: add user controller's functions

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,51 @@
-const getUser = (req, res) => {};
+const prisma = require('../models/queries');
+const bcrypt = require('bcrypt');
+const { validateUser } = require('../utils/validation');
 
-const updateUser = (req, res) => {};
+const getUser = async (req, res) => {
+  const user = await prisma.getUserById(+req.params.userId);
+  if (!user || req.user.id !== +req.params.userId) {
+    return res.sendStatus(404);
+  }
+  res.json(user);
+};
 
-const getUserPosts = (req, res) => {};
+const updateUser = [
+  validateUser,
+  async (req, res) => {
+    const user = await prisma.getUserById(+req.params.userId);
+    console.log(req.user);
+    if (!user || req.user.id !== +req.params.userId) return res.sendStatus(400);
+    let role;
+    if (user.role === 'AUTHOR') {
+      role = 'AUTHOR';
+    } else {
+      role = req.body.role === process.env.AUTHOR_PWD ? 'AUTHOR' : 'USER';
+    }
+    const match = await bcrypt.compare(req.body.password, user.password);
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(503);
+      }
+      await prisma.updateUser(+req.params.userId, {
+        username: req.body.username || user.username,
+        email: req.body.email || user.email,
+        password: match ? user.password : hashedPassword,
+        role,
+      });
+      res.sendStatus(201);
+    });
+  },
+];
+
+const getUserPosts = async (req, res) => {
+  const user = await prisma.getUserById(+req.params.userId);
+  if (!user) {
+    return res.sendStatus(404);
+  }
+  const posts = await prisma.getUserPosts(+req.params.userId);
+  res.json({ posts });
+};
 
 module.exports = { getUser, updateUser, getUserPosts };
