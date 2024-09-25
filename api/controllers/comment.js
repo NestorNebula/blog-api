1bb67fe6b375e7bcbaebe37275633adf0002e498
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-25 10:40:31 +0200

    feat: add getComment function

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -1,4 +1,10 @@
-const getComment = (req, res) => {};
+const prisma = require('../models/queries');
+
+const getComment = async (req, res) => {
+  const comment = await prisma.getCommentById(+req.params.commentId);
+  if (!comment) return res.sendStatus(404);
+  res.json(comment);
+};
 
 const udpateComment = (req, res) => {};
 
