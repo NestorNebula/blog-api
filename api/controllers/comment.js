a9cd08251ac90812acafab2132641a5b4423ff80
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-25 18:22:08 +0200

    feat: add updateComment function

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -1,4 +1,5 @@
 const prisma = require('../models/queries');
+const { validatePost } = require('../utils/validation');
 
 const getComment = async (req, res) => {
   const comment = await prisma.getCommentById(+req.params.commentId);
@@ -6,7 +7,18 @@ const getComment = async (req, res) => {
   res.json(comment);
 };
 
-const udpateComment = (req, res) => {};
+const udpateComment = [
+  validatePost,
+  async (req, res) => {
+    const comment = await prisma.getCommentById(+req.params.commentId);
+    if (!comment) return res.sendStatus(400);
+    if (comment.userId !== req.user.id) return res.sendStatus(403);
+    await prisma.updateComment(+req.params.commentId, {
+      content: req.body.content,
+    });
+    res.sendStatus(201);
+  },
+];
 
 const deleteComment = (req, res) => {};
 
