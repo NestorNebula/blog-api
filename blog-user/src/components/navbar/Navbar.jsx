c39bfce82bd4a56e4e0fc35b29842835cb2c24cc
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-27 09:17:59 +0200

    feat: add Navbar component

---
+++ blog-user/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <div>Homepage</div>
+        </li>
+        <li>
+          <div>Account</div>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
