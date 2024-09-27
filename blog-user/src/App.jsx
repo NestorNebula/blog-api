61045df3fcc02ee5ba759d9cd0f08d5678d07e9e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-27 17:52:54 +0200

    feat: add id state and setId as outlet context

--- blog-user/src/App.jsx
+++ blog-user/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [id, setId] = useState(null);
   return (
     <>
       <Navbar />
-      <Outlet />
+      <Outlet context={{ id, setId }} />
     </>
   );
 }
