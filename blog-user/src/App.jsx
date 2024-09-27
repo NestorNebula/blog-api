511e0970e4ec5c28fdceac95dbb6fef68262a227
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-27 18:30:12 +0200

    feat: add useUser hook in App.jsx
    Once the user is logged in, the hook fetch the up-to-date user infos

--- blog-user/src/App.jsx
+++ blog-user/src/App.jsx
@@ -1,10 +1,34 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+const useUser = (userId) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (userId) {
+      fetch(`http://localhost:3000/users/${userId}`, { mode: 'cors' })
+        .then((response) => {
+          if (response.status >= 400) {
+            throw new Error('Error when fetching data.');
+          }
+          return response.json();
+        })
+        .then((response) => setData(response))
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    }
+  }, [userId]);
+
+  return { data, error, loading };
+};
 
 function App() {
   const [id, setId] = useState(null);
+  const { data: user, error, loading } = useUser();
   return (
     <>
       <Navbar />
