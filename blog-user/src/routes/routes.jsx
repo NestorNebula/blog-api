a9680ca7227d5386c3d8fee12499aa2ae65e309a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-27 09:34:51 +0200

    feat: add LoginForm in routes

--- blog-user/src/routes/routes.jsx
+++ blog-user/src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import App from '../App';
+import LoginForm from '../components/login/LoginForm';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    children: [{ path: 'login', element: <LoginForm /> }],
   },
 ];
 
