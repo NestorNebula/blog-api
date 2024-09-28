fd7426feed33435ee25b17a6adf0bd6fc9e6e58c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-28 15:32:13 +0200

    feat: add loader for App
    The loader will check if the user is authenticated and load its data
    If the user isn't authenticated he will be redirected to auth route

--- blog-user/src/routes/routes.jsx
+++ blog-user/src/routes/routes.jsx
@@ -1,3 +1,5 @@
+import { redirect } from 'react-router-dom';
+import { useUser } from '../hooks/useUser';
 import App from '../App';
 import Auth from '../components/auth/Auth';
 import LoginForm from '../components/login/LoginForm';
@@ -6,6 +8,31 @@ const routes = [
   {
     path: '/',
     element: <App />,
+    loader: () => {
+      const userId = localStorage.getItem('id');
+      if (!userId) return redirect('/auth/login');
+      const { data: user, error } = useUser(userId);
+      if (error) {
+        fetch(
+          'http://localhost:3000/auth/refresh',
+          getFetchOptions('get', null)
+        )
+          .then((response) => {
+            if (response >= 400) {
+              return redirect('/auth/login');
+            }
+            return response.json();
+          })
+          .then((response) => {
+            if (userId === response.id) {
+              setUpdate(!update);
+            } else {
+              return redirect('/auth/login');
+            }
+          });
+      }
+      return user;
+    },
   },
   {
     path: 'auth',
