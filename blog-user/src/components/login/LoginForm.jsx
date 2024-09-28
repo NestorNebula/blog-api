5da6131762c75621909c4b44f6a50fff84c08422
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-28 11:21:04 +0200

    feat: add logic to submit login form

--- blog-user/src/components/login/LoginForm.jsx
+++ blog-user/src/components/login/LoginForm.jsx
@@ -1,8 +1,13 @@
 import { useInput } from '../../hooks/useInput';
 import { verifyUsername, verifyPassword } from '../../helpers/inputValidation';
+import { useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
+import getFetchOptions from '../../helpers/fetchOptions';
 import Input from '../input/Input';
 
 function LoginForm() {
+  const { setId } = useOutletContext();
+  const [error, setError] = useState(null);
   const {
     value: username,
     updateValue: updateUsername,
@@ -15,25 +20,51 @@ function LoginForm() {
     validation: passwordValidation,
   } = useInput(verifyPassword);
 
+  const submitLogIn = async () => {
+    if (
+      !usernameValidation.isValid ||
+      !passwordValidation.isValid ||
+      username.length === 0 ||
+      password.length === 0
+    ) {
+      return;
+    }
+    const response = await fetch(
+      'http://localhost:3000/auth/login',
+      getFetchOptions('post', JSON.stringify({ username, password }))
+    );
+    const result = await response.json();
+    if (response.status >= 400) {
+      setError(result);
+    } else {
+      setId(result.id);
+    }
+  };
+
   return (
-    <form action="http://localhost:3000/auth/login" method="POST">
-      <Input
-        name="username"
-        value={username}
-        update={updateUsername}
-        validation={usernameValidation}
-        type="text"
-        label="Username or Email"
-      />
-      <Input
-        name="password"
-        value={password}
-        update={updatePassword}
-        validation={passwordValidation}
-        type="password"
-      />
-      <button type="button">Log In</button>
-    </form>
+    <>
+      {error && <div>{error}</div>}
+      <form action="http://localhost:3000/auth/login" method="POST">
+        <Input
+          name="username"
+          value={username}
+          update={updateUsername}
+          validation={usernameValidation}
+          type="text"
+          label="Username or Email"
+        />
+        <Input
+          name="password"
+          value={password}
+          update={updatePassword}
+          validation={passwordValidation}
+          type="password"
+        />
+        <button type="button" onClick={submitLogIn}>
+          Log In
+        </button>
+      </form>
+    </>
   );
 }
 
