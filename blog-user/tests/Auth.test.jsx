79fb22ef152f19bade37ee742b52cc4cefbbc30b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-09-29 16:55:14 +0200

    test: add test for LoginForm inputs

--- blog-user/tests/Auth.test.jsx
+++ blog-user/tests/Auth.test.jsx
@@ -25,4 +25,31 @@ describe('LoginForm', () => {
     const button = screen.getByRole('button');
     expect(button.textContent).toBe('Log In');
   });
+
+  it('handles wrong inputs properly', async () => {
+    render(
+      <MemoryRouter>
+        <LoginForm />
+      </MemoryRouter>
+    );
+    const user = userEvent.setup();
+    const usernameInput = screen.getByLabelText(/username/i);
+    await user.type(usernameInput, '.user');
+    expect(
+      screen.queryByText(
+        'Username must start/end with a letter and can only contain letters, numbers, dashes and points.'
+      )
+    ).not.toBeNull();
+    await user.clear(usernameInput);
+    await user.type(usernameInput, 'u');
+    expect(
+      screen.queryByText(/Username\/Email must be at least 3 characters long./)
+    ).not.toBeNull();
+
+    const pwdInput = screen.getByLabelText('password');
+    await user.type(pwdInput, 'Wrong');
+    expect(
+      screen.queryByText('Password must be at least 8 characters long.')
+    ).not.toBeNull();
+  });
 });
