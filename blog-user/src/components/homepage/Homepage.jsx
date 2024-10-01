c68ac9ca290ce246516258e727e0cfcda04d9a92
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-01 14:26:57 +0200

    feat: add content in Homepage

--- blog-user/src/components/homepage/Homepage.jsx
+++ blog-user/src/components/homepage/Homepage.jsx
@@ -2,7 +2,14 @@ import { useState, useEffect } from 'react';
 import { useOutletContext } from 'react-router-dom';
 
 function Homepage() {
-  return <></>;
+  const { user, API_URL } = useOutletContext();
+
+  return (
+    <>
+      <header>Homepage</header>
+      <div>Hello, {user.username} </div>
+    </>
+  );
 }
 
 export default Homepage;
