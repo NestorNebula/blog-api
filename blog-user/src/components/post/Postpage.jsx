e389e5d0f5e42bc540afb9538ddd6c343281948d
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-01 18:10:08 +0200

    feat: create Postpage component

---
+++ blog-user/src/components/post/Postpage.jsx
@@ -0,0 +1,7 @@
+import { useOutletContext } from 'react-router-dom';
+import { useData } from '../../hooks/useData';
+import useFetchOptions from '../../helpers/fetchOptions';
+
+function Postpage() {}
+
+export default Postpage;
