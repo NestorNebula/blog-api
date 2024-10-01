143fccf2b25dec30c07f2e154534c5104ba375f4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-01 16:43:47 +0200

    feat: add Homepage to app route

--- blog-user/src/routes/routes.jsx
+++ blog-user/src/routes/routes.jsx
@@ -2,12 +2,19 @@ import App from '../App';
 import Auth from '../components/auth/Auth';
 import LoginForm from '../components/auth/login/LoginForm';
 import SignupForm from '../components/auth/signup/SignupForm';
+import Homepage from '../components/homepage/Homepage';
 import { rootLoader, authLoader } from '../helpers/loaders';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+    ],
     loader: async () => await rootLoader(),
   },
   {
