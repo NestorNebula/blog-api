10612cf02d9c8b12ec40d9ea90e04f821934d953
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-01 18:22:27 +0200

    refactor: move faker functions in their own file

---
+++ blog-user/src/helpers/faker.js
@@ -0,0 +1,35 @@
+import { faker } from '@faker-js/faker';
+
+const getFakePost = () => {
+  const postId = faker.number.int({ max: 1000 });
+  const userId = faker.number.int({ max: 100 });
+
+  return {
+    id: postId,
+    title: faker.word.words({ max: 5 }),
+    content: faker.lorem.paragraphs({ max: 5 }),
+    creationDate: faker.date.recent(),
+    upDate: new Date(Date.now()),
+    published: true,
+    userId: userId,
+    user: {
+      username: faker.person.firstName(),
+    },
+    Comments: [getFakeComment(postId, userId), getFakeComment(postId, userId)],
+  };
+};
+
+const getFakeComment = (postId, userId) => {
+  return {
+    id: faker.number.int({ max: 10000 }),
+    content: faker.lorem.paragraph({ max: 3 }),
+    creationDate: faker.date.recent(),
+    postId,
+    userId,
+    user: {
+      username: faker.person.firstName(),
+    },
+  };
+};
+
+export { getFakePost, getFakeComment };
--- blog-user/tests/Homepage.test.jsx
+++ blog-user/tests/Homepage.test.jsx
@@ -3,36 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import Homepage from '../src/components/homepage/Homepage';
-import { faker } from '@faker-js/faker';
-
-const getFakePost = () => {
-  const postId = faker.number.int({ max: 1000 });
-  const userId = faker.number.int({ max: 100 });
-
-  return {
-    id: postId,
-    title: faker.word.words({ max: 5 }),
-    content: faker.lorem.paragraphs({ max: 5 }),
-    creationDate: faker.date.recent(),
-    upDate: new Date(Date.now()),
-    published: true,
-    userId: userId,
-    user: {
-      username: faker.person.firstName(),
-    },
-    Comments: [getFakeComment(postId, userId), getFakeComment(postId, userId)],
-  };
-};
-
-const getFakeComment = (postId, userId) => {
-  return {
-    id: faker.number.int({ max: 10000 }),
-    content: faker.lorem.paragraph({ max: 3 }),
-    creationDate: faker.date.recent(),
-    postId,
-    userId,
-  };
-};
+import { getFakePost } from '../src/helpers/faker';
 
 vi.mock('../src/hooks/useData', () => {
   return {
