91aca9ecbf0c1e353d0e7fb499f9a83aae7071f5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-01 17:15:08 +0200

    feat: display all posts in Homepage

--- blog-user/src/components/homepage/Homepage.jsx
+++ blog-user/src/components/homepage/Homepage.jsx
@@ -1,13 +1,42 @@
-import { useState, useEffect } from 'react';
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, Link } from 'react-router-dom';
+import { useData } from '../../hooks/useData';
+import getFetchOptions from '../../helpers/fetchOptions';
 
 function Homepage() {
   const { user, API_URL } = useOutletContext();
+  const {
+    data: posts,
+    loading,
+    error,
+  } = useData(`${API_URL}/posts`, getFetchOptions('get', null));
 
   return (
     <>
-      <header>Homepage</header>
-      <div>Hello, {user.username} </div>
+      <header>
+        <div>Homepage</div>
+        <div>{`Hello, ${user.username}`}</div>
+      </header>
+      {error ? (
+        <div>Error when loading data.</div>
+      ) : loading ? (
+        <div>Loading data...</div>
+      ) : (
+        <section>
+          {posts.map((post) => {
+            return (
+              <div key={post.id}>
+                <div>{post.user.username}</div>
+                <div>{post.title}</div>
+                <div>{new Date(post.creationDate).toLocaleDateString()}</div>
+                <div>{post.content}</div>
+                <Link
+                  to={`/posts/${post.id}`}
+                >{`${post.Comments.length} comments`}</Link>
+              </div>
+            );
+          })}
+        </section>
+      )}
     </>
   );
 }
