26f08f9451b682b71b3b8c3ac3b986936f6e7b55
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-02 10:45:09 +0200

    feat: add possibility to comment a post

--- blog-user/src/components/post/Postpage.jsx
+++ blog-user/src/components/post/Postpage.jsx
@@ -3,6 +3,7 @@ import { useOutletContext, useParams } from 'react-router-dom';
 import { useData } from '../../hooks/useData';
 import getFetchOptions from '../../helpers/fetchOptions';
 import Post from './Post';
+import Input from '../input/Input';
 
 function Postpage() {
   const { user, API_URL } = useOutletContext();
@@ -15,6 +16,26 @@ function Postpage() {
   } = useData(`${API_URL}/posts/${postId}`, getFetchOptions('get', null), [
     update,
   ]);
+  const [comment, setComment] = useState('');
+  const updateComment = (e) => {
+    setComment(e.target.value);
+  };
+
+  const [postError, setPostError] = useState(null);
+  const postComment = async () => {
+    if (!comment.length) return;
+    const response = await fetch(
+      `${API_URL}/posts/${postId}/comments`,
+      getFetchOptions('post', JSON.stringify({ content: comment, postId }))
+    );
+    if (response.status >= 400) {
+      setPostError('Error when posting comment.');
+    } else {
+      setPostError(null);
+      setUpdate(!update);
+      setComment('');
+    }
+  };
 
   return (
     <>
@@ -25,7 +46,14 @@ function Postpage() {
       ) : (
         <>
           <Post post={post} details={true} />
-          <button>Add Comment</button>
+          {postError && <div>{postError}</div>}
+          <Input
+            name="comment"
+            value={comment}
+            update={updateComment}
+            validation={{ isValid: true }}
+          />
+          <button onClick={postComment}>Post Comment as {user.username}</button>
         </>
       )}
     </>
