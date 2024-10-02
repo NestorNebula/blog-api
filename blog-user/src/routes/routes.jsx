a416e5fba1c505c2bca60317c6aa62a0ecd4300f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-02 18:07:38 +0200

    feat: add Account component in app route

--- blog-user/src/routes/routes.jsx
+++ blog-user/src/routes/routes.jsx
@@ -4,6 +4,7 @@ import LoginForm from '../components/auth/login/LoginForm';
 import SignupForm from '../components/auth/signup/SignupForm';
 import Homepage from '../components/homepage/Homepage';
 import Postpage from '../components/post/Postpage';
+import Account from '../components/account/Account';
 import { rootLoader, authLoader } from '../helpers/loaders';
 
 const routes = [
@@ -18,6 +19,10 @@ const routes = [
       {
         path: 'posts/:postId',
         element: <Postpage />,
+      },
+      {
+        path: 'account',
+        element: <Account />,
       },
     ],
     loader: async () => await rootLoader(),
