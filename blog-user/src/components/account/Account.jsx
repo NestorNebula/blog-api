e0bd761b8fac7b3d54161130c41ab0d439dc7091
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-02 16:02:29 +0200

    feat: display user informations on Account

--- blog-user/src/components/account/Account.jsx
+++ blog-user/src/components/account/Account.jsx
@@ -1,3 +1,19 @@
-function Account() {}
+import { useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import getFetchOptions from '../../helpers/fetchOptions';
+
+function Account() {
+  const { user, API_URL } = useOutletContext();
+
+  return (
+    <main>
+      <section>
+        <div>{user.username}</div>
+        <div> {user.email} </div>
+        <div> {user.role.toLowerCase()}</div>
+      </section>
+    </main>
+  );
+}
 
 export default Account;
