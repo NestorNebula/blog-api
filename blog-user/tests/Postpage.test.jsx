2c50c42ef92e84a27edcd6a4f3611922853ee610
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-02 09:37:44 +0200

    test: add needed mocks to Postpage tests

--- blog-user/tests/Postpage.test.jsx
+++ blog-user/tests/Postpage.test.jsx
@@ -7,6 +7,39 @@ import Postpage from '../src/components/post/Postpage';
 
 const post = getFakePost();
 
+vi.mock('../src/hooks/useData', () => {
+  return {
+    useData: () => {
+      return {
+        data: post,
+        error: null,
+        loading: false,
+      };
+    },
+  };
+});
+
+vi.mock('react-router-dom', async () => {
+  const actual = await vi.importActual('react-router-dom');
+  return {
+    ...actual,
+    useOutletContext: vi.fn(() => {
+      return {
+        user: {
+          id: 1,
+          username: 'username',
+          email: 'this@email.com',
+          role: 'USER',
+        },
+        API_URL: null,
+      };
+    }),
+    useParams: vi.fn(() => {
+      return { postId: post.id };
+    }),
+  };
+});
+
 describe('Postpage', () => {
   it('renders postpage', () => {
     render(
