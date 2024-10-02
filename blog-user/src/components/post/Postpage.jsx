ae65a0992eb54157ade45c4f340e4cd5393dce96
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-02 09:10:13 +0200

    feat: add post content to Postpage

--- blog-user/src/components/post/Postpage.jsx
+++ blog-user/src/components/post/Postpage.jsx
@@ -1,7 +1,32 @@
-import { useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
+import { useOutletContext, useParams } from 'react-router-dom';
 import { useData } from '../../hooks/useData';
-import useFetchOptions from '../../helpers/fetchOptions';
+import getFetchOptions from '../../helpers/fetchOptions';
+import Post from './Post';
 
-function Postpage() {}
+function Postpage() {
+  const { user, API_URL } = useOutletContext();
+  const { postId } = useParams();
+  const [update, setUpdate] = useState(false);
+  const {
+    data: post,
+    error,
+    loading,
+  } = useData(`${API_URL}/posts/${postId}`, getFetchOptions('get', null), [
+    update,
+  ]);
+
+  return (
+    <>
+      {error ? (
+        <div>Error when loading post.</div>
+      ) : loading ? (
+        <div>Loading post...</div>
+      ) : (
+        <Post post={post} details={true} />
+      )}
+    </>
+  );
+}
 
 export default Postpage;
