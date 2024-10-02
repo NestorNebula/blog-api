6520d7460339ac1ee021de8eb2a139daa7abe679
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-02 11:17:33 +0200

    feat: add Postpage test for deleting own comment

--- blog-user/tests/Postpage.test.jsx
+++ blog-user/tests/Postpage.test.jsx
@@ -1,4 +1,4 @@
-import { vi, describe, it, expect } from 'vitest';
+import { vi, describe, it, expect, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
@@ -6,6 +6,14 @@ import { getFakePost } from '../src/helpers/faker';
 import Postpage from '../src/components/post/Postpage';
 
 const post = getFakePost();
+
+beforeEach(() => {
+  render(
+    <MemoryRouter>
+      <Postpage />
+    </MemoryRouter>
+  );
+});
 
 vi.mock('../src/hooks/useData', () => {
   return {
@@ -26,7 +34,7 @@ vi.mock('react-router-dom', async () => {
     useOutletContext: vi.fn(() => {
       return {
         user: {
-          id: 1,
+          id: post.Comments[0].userId,
           username: 'username',
           email: 'this@email.com',
           role: 'USER',
@@ -42,39 +50,39 @@ vi.mock('react-router-dom', async () => {
 
 describe('Postpage', () => {
   it('renders postpage', () => {
-    render(
-      <MemoryRouter>
-        <Postpage />
-      </MemoryRouter>
-    );
-    expect(screen.queryByRole('button')).not.toBeNull();
+    expect(screen.queryByRole('button', { name: /post/i })).not.toBeNull();
   });
 
   it('renders post', () => {
-    render(
-      <MemoryRouter>
-        <Postpage />
-      </MemoryRouter>
-    );
     expect(screen.queryByText(post.title)).not.toBeNull();
   });
 
   it('add comment', async () => {
-    render(
-      <MemoryRouter>
-        <Postpage />
-      </MemoryRouter>
-    );
     globalThis.fetch = vi.fn((url, options) => {
       return {
         status: 201,
       };
     });
     const user = userEvent.setup();
-    const btn = screen.getByRole('button');
+    const btn = screen.getByRole('button', { name: /post/i });
     const content = screen.getByLabelText(/comment/i);
     await user.type(content, 'a comment');
     await user.click(btn);
     expect(globalThis.fetch).toHaveBeenCalled();
+  });
+
+  it('let user remove its comment', async () => {
+    globalThis.fetch = vi.fn((url, options) => {
+      return {
+        status: 200,
+        url,
+      };
+    });
+    const user = userEvent.setup();
+    const btn = screen.getByRole('button', { name: /delete/i });
+    expect(btn).not.toBeNull();
+    await user.click(btn);
+    const url = globalThis.fetch.mock.results[0].url;
+    expect(url).toMatch(post.Comments[0].id);
   });
 });
