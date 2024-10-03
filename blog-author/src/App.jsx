48c51fad6f3f29d7c514e3060c188bd91f3b5540
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-03 18:11:33 +0200

    feat: add content in App
    The app display the normal content if the user is an author
    If the user isn't an author, he will have links to blog-user app

--- blog-author/src/App.jsx
+++ blog-author/src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import { Link, useLoaderData, Outlet } from 'react-router-dom';
+const BLOG_URL = import.meta.env.VITE_BLOG_URL;
 
 function App() {
+  const { user } = useLoaderData();
   return (
     <>
       <Navbar></Navbar>
+      {user.role === 'AUTHOR' ? (
+        <Outlet />
+      ) : (
+        <section>
+          <div>This page is for the blog authors.</div>
+          <div>
+            Please go to the <Link to={`${BLOG_URL}`}>user website</Link> to
+            browse through the Blog.
+          </div>
+        </section>
+      )}
     </>
   );
 }
