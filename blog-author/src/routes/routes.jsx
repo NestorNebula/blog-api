8bdb007d2b81f10fb82250a8c303c3e1d3d6325a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-03 16:17:27 +0200

    feat: add root loader for root path

--- blog-author/src/routes/routes.jsx
+++ blog-author/src/routes/routes.jsx
@@ -1,9 +1,11 @@
 import App from '../App';
+import { rootLoader, authLoader } from '../helpers/loaders';
 
 const routes = [
   {
     path: '/',
     element: <App />,
+    loader: async () => await rootLoader(),
   },
 ];
 
