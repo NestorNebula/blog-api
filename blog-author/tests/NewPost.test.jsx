7cfc7a783ac879a726faa4cc14212aa73c5d457a
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 15:04:09 +0200

    test: add test for NewPost component

--- blog-author/tests/NewPost.test.jsx
+++ blog-author/tests/NewPost.test.jsx
@@ -2,10 +2,11 @@ import { beforeEach, describe, it, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
-import { getFakeAuthor } from '../src/helpers/faker';
+import { getFakeAuthor, getFakePost } from '../src/helpers/faker';
 import NewPost from '../src/components/newpost/NewPost';
 
 const author = getFakeAuthor();
+const post = getFakePost();
 
 vi.mock('react-router-dom', async () => {
   const actual = await vi.importActual('react-router-dom');
@@ -30,5 +31,30 @@ beforeEach(() => {
 describe('NewPost', () => {
   it('renders correctly', () => {
     expect(screen.queryByText(/create new post/i)).not.toBeNull();
+  });
+
+  it('submit new post', async () => {
+    globalThis.fetch = vi.fn((url, options) => {
+      return {
+        status: 201,
+        url,
+        options,
+      };
+    });
+
+    const user = userEvent.setup();
+    const title = screen.getByLabelText(/title/i);
+    const content = screen.getByLabelText(/content/i);
+    const published = screen.getByLabelText(/publish/i);
+    await user.type(title, post.title);
+    await user.type(content, post.content);
+    await user.click(published);
+    const submit = screen.getByRole('button', { name: /create/i });
+    await user.click(submit);
+    expect(JSON.parse(globalThis.fetch.mock.calls[0][1].body)).toEqual({
+      title: post.title,
+      content: post.content,
+      published: true,
+    });
   });
 });
