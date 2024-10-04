062baeb360d2cbb2cfbf634395737e2fe824241b
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 10:50:10 +0200

    feat: create PostManager component

---
+++ blog-author/src/components/post/PostManager.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useData } from '../../hooks/useData';
+import getFetchOptions from '../../helpers/fetchOptions';
+import Post from './Post';
+
+function PostManager() {}
+
+export default PostManager;
