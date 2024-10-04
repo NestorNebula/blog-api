2df5e5786bf575db3b2459a1a1e349d6da9d8cfc
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 10:01:38 +0200

    feat: add Post component from blog-user

---
+++ blog-author/src/components/post/Post.jsx
@@ -0,0 +1,27 @@
+import Comment from '../comment/Comment';
+
+function Post({ post, details }) {
+  return (
+    <div>
+      <div>{post.user.username}</div>
+      <div>{post.title}</div>
+      <div>{new Date(post.creationDate)}</div>
+      <div>{post.content}</div>
+      {details ? (
+        <div>
+          {post.Comments.map((comment) => {
+            return <Comment key={comment.id} comment={comment} />;
+          })}
+        </div>
+      ) : details !== false ? (
+        <div>
+          {post.Comments.length > 1
+            ? `${post.Comments.length} comments`
+            : `${post.Comments.length} comment`}
+        </div>
+      ) : null}
+    </div>
+  );
+}
+
+export default Post;
