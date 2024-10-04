cf5c6eb59d2ff079a3f839bb1ef5092226580a0e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 16:05:39 +0200

    feat: add NewPost component to App route

--- blog-author/src/routes/routes.jsx
+++ blog-author/src/routes/routes.jsx
@@ -2,6 +2,7 @@ import App from '../App';
 import LoginForm from '../components/login/LoginForm';
 import Dashboard from '../components/dashboard/Dashboard';
 import PostManager from '../components/post/PostManager';
+import NewPost from '../components/newpost/NewPost';
 import { rootLoader, authLoader } from '../helpers/loaders';
 
 const routes = [
@@ -17,6 +18,10 @@ const routes = [
       {
         path: '/posts/:postId',
         element: <PostManager />,
+      },
+      {
+        path: 'new',
+        element: <NewPost />,
       },
     ],
   },
