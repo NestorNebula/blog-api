dfe8a96dd1ee7ed71d13bb69d2f3cbf473faccc8
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 13:07:43 +0200

    feat: add PostManager to App route

--- blog-author/src/routes/routes.jsx
+++ blog-author/src/routes/routes.jsx
@@ -1,6 +1,7 @@
 import App from '../App';
 import LoginForm from '../components/login/LoginForm';
 import Dashboard from '../components/dashboard/Dashboard';
+import PostManager from '../components/post/PostManager';
 import { rootLoader, authLoader } from '../helpers/loaders';
 
 const routes = [
@@ -12,6 +13,10 @@ const routes = [
       {
         index: true,
         element: <Dashboard />,
+      },
+      {
+        path: '/posts/:postId',
+        element: <PostManager />,
       },
     ],
   },
