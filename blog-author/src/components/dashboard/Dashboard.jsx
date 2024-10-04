49b26a735d1c8e77d8464d335985051bf4363cca
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 09:19:59 +0200

    feat: create Dashboard component

---
+++ blog-author/src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,7 @@
+import { useOutletContext, Link } from 'react-router-dom';
+import { useData } from '../../hooks/useData';
+import getFetchOptions from '../../helpers/fetchOptions';
+
+function Dashboard() {}
+
+export default Dashboard;
