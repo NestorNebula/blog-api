6f8f8482f8ae71d1be99f0475c833f0e730f04cf
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 13:20:41 +0200

    feat: create NewPost component

---
+++ blog-author/src/components/newpost/NewPost.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import { useInput } from '../../hooks/useInput';
+import getFetchOptions from '../../helpers/fetchOptions';
+import Input from '../input/Input';
+const API_URL = import.meta.env.VITE_API_URL;
+
+function NewPost() {}
+
+export default NewPost;
