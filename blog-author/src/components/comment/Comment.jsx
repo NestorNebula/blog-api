9f6cf91333836c3938a8d8cb53bdf031bb717b69
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 10:08:51 +0200

    feat: add Comment component from blog-user
    The component has been updated to be used by the author

---
+++ blog-author/src/components/comment/Comment.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import getFetchOptions from '../../helpers/fetchOptions';
+const API_URL = import.meta.env.VITE_API_URL;
+
+function Comment({ comment }) {
+  const [deleteChoice, setDeleteChoice] = useState(false);
+  const updateDeleteChoice = () => {
+    setDeleteChoice(!deleteChoice);
+  };
+
+  const deleteComment = async () => {
+    await fetch(
+      `${API_URL}/comments/${comment.id}`,
+      getFetchOptions('delete', null)
+    );
+    setDeleteChoice(false);
+    setUpdate(!update);
+  };
+
+  return (
+    <div>
+      <div>{comment.user.username}</div>
+      <div>{comment.content}</div>
+      <div>{new Date(comment.creationDate)}</div>
+      <button onClick={updateDeleteChoice}>Delete</button>
+      {deleteChoice && (
+        <div>
+          <div>
+            Are you sure you want to delete this comment ? This cannot be
+            undone.
+          </div>
+          <button onClick={deleteComment}>Confirm</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Comment;
