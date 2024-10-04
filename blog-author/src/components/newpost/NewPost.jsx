218fef6a9a7095d29e6b5b5446a76b06a3eb16be
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 13:33:17 +0200

    feat: add basis content to NewPost component

--- blog-author/src/components/newpost/NewPost.jsx
+++ blog-author/src/components/newpost/NewPost.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, Navigate } from 'react-router-dom';
 import { useInput } from '../../hooks/useInput';
 import getFetchOptions from '../../helpers/fetchOptions';
 import Input from '../input/Input';
 const API_URL = import.meta.env.VITE_API_URL;
 
-function NewPost() {}
+function NewPost() {
+  const { author } = useOutletContext();
+  const [submit, setSubmit] = useState(false);
+
+  return (
+    <main>
+      {submit && <Navigate to="/" />}
+      <header>
+        <div>Create New Post</div>
+        <div>as {author.username}</div>
+      </header>
+      <section></section>
+    </main>
+  );
+}
 
 export default NewPost;
