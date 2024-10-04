fcea1349ad32ec6d09aa9f4ac07a56ccc451e9c5
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-04 10:14:45 +0200

    feat: add content to Dashboard component

--- blog-author/src/components/dashboard/Dashboard.jsx
+++ blog-author/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,46 @@
 import { useOutletContext, Link } from 'react-router-dom';
 import { useData } from '../../hooks/useData';
 import getFetchOptions from '../../helpers/fetchOptions';
+import Post from '../post/Post';
+const API_URL = import.meta.env.VITE_API_URL;
 
-function Dashboard() {}
+function Dashboard() {
+  const { author } = useOutletContext();
+  const {
+    data: posts,
+    error,
+    loading,
+  } = useData(
+    `${API_URL}/users/${author.id}/posts`,
+    getFetchOptions('get', null)
+  );
+
+  return (
+    <>
+      {error ? (
+        <div>Error when loading posts.</div>
+      ) : loading ? (
+        <div>Loading posts...</div>
+      ) : (
+        <main>
+          <header>
+            <div>{`${author.username}'s Dashboard`}</div>
+          </header>
+          <section>
+            <div>Posts</div>
+            {posts.map((post) => {
+              return (
+                <div key={post.id}>
+                  <Post post={post} />
+                  <Link to={`posts/${post.id}`}>See details</Link>
+                </div>
+              );
+            })}
+          </section>
+        </main>
+      )}
+    </>
+  );
+}
 
 export default Dashboard;
