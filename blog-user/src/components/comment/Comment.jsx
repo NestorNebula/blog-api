222a9cfb48177575b66f656705a17f0f899f7598
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-05 17:18:03 +0200

    feat: add styles for Comment component

--- blog-user/src/components/comment/Comment.jsx
+++ blog-user/src/components/comment/Comment.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import getFetchOptions from '../../helpers/fetchOptions';
 import { formatDistanceToNow } from 'date-fns';
+import styles from './Comment.module.css';
+import trash from '../../assets/icons/trash.png';
 
 function Comment({ comment, user, update, setUpdate, API_URL }) {
   const [deleteChoice, setDeleteChoice] = useState(false);
@@ -18,15 +20,19 @@ function Comment({ comment, user, update, setUpdate, API_URL }) {
   };
 
   return (
-    <div>
-      <div>{comment.user.username}</div>
-      <div>{comment.content}</div>
-      <div>{formatDistanceToNow(comment.creationDate) + ' ago'}</div>
+    <div className={styles.comment}>
+      <div className={styles.commentUsername}>{comment.user.username}</div>
+      <div className={styles.commentDate}>
+        {formatDistanceToNow(comment.creationDate) + ' ago'}
+      </div>
+      <div className={styles.commentContent}>{comment.content}</div>
       {user && user.id === comment.userId && (
-        <button onClick={updateDeleteChoice}>Delete</button>
+        <button className={styles.commentDelete} onClick={updateDeleteChoice}>
+          <img src={trash} alt="Delete" />
+        </button>
       )}
       {deleteChoice && (
-        <div>
+        <div className={styles.commentConfirm}>
           <div>
             Are you sure you want to delete this comment ? This cannot be
             undone.
