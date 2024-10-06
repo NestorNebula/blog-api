23cccbe597bf176940258b1a35786ae990082c73
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-06 15:46:46 +0200

    feat: add styles for Loading component

--- blog-author/src/components/loading/Loading.jsx
+++ blog-author/src/components/loading/Loading.jsx
@@ -1,6 +1,8 @@
+import styles from './Loading.module.css';
+
 function Loading() {
   return (
-    <section>
+    <section className={styles.loading}>
       <div>We are loading the data.</div>
       <div>Please wait...</div>
     </section>
