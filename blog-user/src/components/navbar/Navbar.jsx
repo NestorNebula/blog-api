1cad846009f36e93af907ab6f8591b775a0de73c
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-06 09:31:29 +0200

    feat: add icons and more styles to Navbar

--- blog-user/src/components/navbar/Navbar.jsx
+++ blog-user/src/components/navbar/Navbar.jsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import home from '../../assets/icons/home.svg';
+import account from '../../assets/icons/account.svg';
 
 function Navbar() {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Homepage</Link>
+          <Link to="/">
+            <img className={styles.icon} src={home} alt="homepage" />
+          </Link>
         </li>
         <li>
-          <Link to="/account">Account</Link>
+          <div className={styles.blog}>Blog</div>
+        </li>
+        <li>
+          <Link to="/account">
+            <img className={styles.icon} src={account} alt="account" />
+          </Link>
         </li>
       </ul>
     </nav>
