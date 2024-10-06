0c97ea5d5bf0a9c3b8a58d575c35d19642fedf4f
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-06 15:42:08 +0200

    feat: add styles to the LoginForm component

--- blog-author/src/components/login/LoginForm.jsx
+++ blog-author/src/components/login/LoginForm.jsx
@@ -7,6 +7,7 @@ import Input from '../input/Input';
 import Navbar from '../navbar/Navbar';
 const API_URL = import.meta.env.VITE_API_URL;
 const BLOG_URL = import.meta.env.VITE_BLOG_URL;
+import styles from './Login.module.css';
 
 function LoginForm() {
   const [success, setSuccess] = useState(false);
@@ -49,39 +50,45 @@ function LoginForm() {
       <Navbar />
       {success && <Navigate to="/" />}
       {error && <div>{error}</div>}
-      <form action="" method="POST">
-        <Input
-          name="username"
-          value={username}
-          update={updateUsername}
-          validation={usernameValidation}
-          type="text"
-          label="Username or Email"
-        />
-        <Input
-          name="password"
-          value={password}
-          update={updatePassword}
-          validation={passwordValidation}
-          type="password"
-        />
-        <button type="button" onClick={submitLogIn}>
-          Log In
-        </button>
-      </form>
-      <div>
-        <div>
-          Please keep in mind that to access the website, you need to have an
-          "Author" account.
+      <section className={styles.loginSection}>
+        <form action="" method="POST" className={styles.loginForm}>
+          <Input
+            name="username"
+            value={username}
+            update={updateUsername}
+            validation={usernameValidation}
+            type="text"
+            label="Username or Email"
+          />
+          <Input
+            name="password"
+            value={password}
+            update={updatePassword}
+            validation={passwordValidation}
+            type="password"
+          />
+          <button type="button" onClick={submitLogIn}>
+            Log In
+          </button>
+        </form>
+        <div className={styles.loginInfos}>
+          <div>
+            <div>
+              Please keep in mind that to access the website, you need to have
+              an "Author" account.
+            </div>
+            <Link className={styles.loginLink} to={`${BLOG_URL}`}>
+              User Website
+            </Link>
+          </div>
+          <div>
+            <div>Don't have an account yet ?</div>
+            <Link className={styles.loginLink} to={`${BLOG_URL}/auth/signup`}>
+              <div>Sign Up</div>
+            </Link>
+          </div>
         </div>
-        <Link to={`${BLOG_URL}`}>User Website</Link>
-      </div>
-      <div>
-        <div>Don't have an account yet ?</div>
-        <Link to={`${BLOG_URL}/auth/signup`}>
-          <div>Sign Up</div>
-        </Link>
-      </div>
+      </section>
     </>
   );
 }
