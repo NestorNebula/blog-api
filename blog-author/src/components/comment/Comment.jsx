65e2c4d53d576ddd91abd33bcc075f9ffd7746be
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-10-06 10:57:26 +0200

    feat: add styles and icon to Comment component

--- blog-author/src/components/comment/Comment.jsx
+++ blog-author/src/components/comment/Comment.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import getFetchOptions from '../../helpers/fetchOptions';
 const API_URL = import.meta.env.VITE_API_URL;
+import styles from './Comment.module.css';
+import trash from '../../assets/icons/trash.png';
 
 function Comment({ comment, update, setUpdate }) {
   const [deleteChoice, setDeleteChoice] = useState(false);
@@ -18,13 +20,17 @@ function Comment({ comment, update, setUpdate }) {
   };
 
   return (
-    <div>
-      <div>{comment.user.username}</div>
-      <div>{comment.content}</div>
-      <div>{new Date(comment.creationDate).toLocaleString()}</div>
-      <button onClick={updateDeleteChoice}>Delete</button>
+    <div className={styles.comment}>
+      <div className={styles.commentUsername}>{comment.user.username}</div>
+      <div className={styles.commentDate}>
+        {new Date(comment.creationDate).toLocaleString()}
+      </div>
+      <div className={styles.commentContent}>{comment.content}</div>
+      <button className={styles.commentDelete} onClick={updateDeleteChoice}>
+        <img src={trash} alt="delete" />
+      </button>
       {deleteChoice && (
-        <div>
+        <div className={styles.commentConfirm}>
           <div>
             Are you sure you want to delete this comment ? This cannot be
             undone.
